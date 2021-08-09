a8af36177c9ce4ec9b5f2f220c9abd059c5180a2
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-09 13:16:27 -0300

    initial commit
    guardar archivos iniciales

---
+++ scriptlibros.js
@@ -0,0 +1,26 @@
+const list_url = "https://vivianabm.github.io/libros/listado.json";
+
+function cargarDatos(url) {
+    document.getElementById("data").innerHTML = "";
+    fetch(url)
+        .then(respuesta => respuesta.json())
+
+        .then(datos => {
+           for (let i = 0; i < datos.length; i++) {
+              
+                let row = "";
+                row=`
+                <tr>
+                <td>`+ datos[i].titulo + `</td>
+                <td>`+ datos[i].autor + `</td>
+                <td>`+ datos[i].editorial + `</td>
+                <td>`+ datos[i].categoria + `</td>
+                </tr>
+                `;
+                document.getElementById("data").innerHTML += row;
+            }
+          
+
+        })
+        .catch(error => alert("Hubo un error: " + error));
+}
