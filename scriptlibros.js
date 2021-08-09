2a84d92d495fc8d2d2a48655699d711404efb060
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-09 14:24:47 -0300

    Update scriptlibros.js

--- scriptlibros.js
+++ scriptlibros.js
@@ -1,20 +1,18 @@
 const list_url = "https://vivianabm.github.io/pagina-libros/listado.json";
-
 function cargarDatos(url) {
     document.getElementById("data").innerHTML = "";
     fetch(url)
         .then(respuesta => respuesta.json())
-
-        .then(datos => {
-           for (let i = 0; i < datos.length; i++) {
+.then(listado => {
+           for (let i = 0; i < listado.length; i++) {
               
                 let row = "";
                 row=`
                 <tr>
-                <td>`+ datos[i].titulo + `</td>
-                <td>`+ datos[i].autor + `</td>
-                <td>`+ datos[i].editorial + `</td>
-                <td>`+ datos[i].categoria + `</td>
+                <td>`+ listado[i].titulo + `</td>
+                <td>`+ listado[i].autor + `</td>
+                <td>`+ listado[i].editorial + `</td>
+                <td>`+ listado[i].categoria + `</td>
                 </tr>
                 `;
                 document.getElementById("data").innerHTML += row;
